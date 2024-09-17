d174f2e6935cd9130d7c29141b6e27f40ada5182
alexburry <alexjburry@gmail.com>
2024-09-17 22:50:11 +0100

    add save to pdf

--- src/components/markdown-converter.ts
+++ src/components/markdown-converter.ts
@@ -1,6 +1,7 @@
 import {unified} from 'unified'
 import remarkParse from 'remark-parse'
 import remarkHtml from 'remark-html'
+import jsPDF from 'jspdf'
 
 function MarkdownConverter(buffer : string) {
   unified()
@@ -8,7 +9,21 @@ function MarkdownConverter(buffer : string) {
   .use(remarkHtml)
   .process(buffer)
   .then((file) => {
-    console.log(String(file))
+    const htmlContent = String(file);
+    console.log(htmlContent)
+
+    const doc = new jsPDF("p", "pt", [500, 600]);
+
+    doc.html(`<div style="width:1350px">${htmlContent}</div>`, {
+      callback: (pdf) => {
+        pdf.save("MarkdownToPdf.pdf");
+
+      },
+      margin: [10, 10, 10, 10],
+    });
+  })
+  .catch((e) => { 
+    console.error("Error generating PDF", e);
   })
 }
 
