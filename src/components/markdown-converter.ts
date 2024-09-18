8bc7df9bead4889a53c7550e41c17ff077a15594
alexburry <alexjburry@gmail.com>
2024-09-18 23:14:47 +0100

    update to html2canvas

--- src/components/markdown-converter.ts
+++ src/components/markdown-converter.ts
@@ -2,6 +2,7 @@ import {unified} from 'unified'
 import remarkParse from 'remark-parse'
 import remarkHtml from 'remark-html'
 import jsPDF from 'jspdf'
+import html2canvas from 'html2canvas'
 
 function MarkdownConverter(buffer : string) {
   unified()
@@ -10,21 +11,56 @@ function MarkdownConverter(buffer : string) {
   .process(buffer)
   .then((file) => {
     const htmlContent = String(file);
-    console.log(htmlContent)
 
-    const doc = new jsPDF("p", "pt", [500, 600]);
+    const markdownStyles = `
+        <style>
+          body {
+            font-family: Arial, sans-serif;
+            color: #333;
+          }
+          h1 {
+            color: #00509e;
+            font-size: 24px;
+          }
+          p {
+            font-size: 14px;
+            line-height: 1.6;
+          }
+          code {
+            background-color: #f4f4f4;
+            padding: 2px 4px;
+            border-radius: 4px;
+            font-family: 'Courier New', monospace;
+          }
+        </style>
+      `;
+    const container = document.createElement('div');
+    container.innerHTML = `
+      <html>
+        <head>${markdownStyles}</head>
+        <body>
+          <div>${htmlContent}</div>
+        </body>
+      </html>`
+      container.style.position = 'absoulte';
+      container.style.left = '-9999px'
+      document.body.appendChild(container);
 
-    doc.html(`<div style="width:1350px">${htmlContent}</div>`, {
-      callback: (pdf) => {
-        pdf.save("MarkdownToPdf.pdf");
+    html2canvas(container).then((canvas) => {
+      const imgData = canvas.toDataURL('image/png');
+      const doc = new jsPDF("p", "pt", [500, 600]);
 
-      },
-      margin: [10, 10, 10, 10],
+      doc.addImage(imgData, 'PNG', 10, 10, canvas.width /2, canvas.height /2);
+      doc.save('MarkdownToPdf.pdf')
+
+      document.body.removeChild(container)
+    }).catch((e) => {
+      console.error('Error rendering canvas', e)
     });
   })
   .catch((e) => { 
     console.error("Error generating PDF", e);
-  })
+  });
 }
 
 export default MarkdownConverter;
